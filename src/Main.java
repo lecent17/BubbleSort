3b9c5ea0426ccbfe03f10191d961756b530ae2d4
Zahar Ulyashov <pelfox.za@gmail.com>
2022-11-12 00:12:30 +0300

    Добавил конструктор класса

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 class BubbleSort {
 
+    BubbleSort (int arr[]) {
+        bubbleSortArray(arr);
+    }
+
     //вывод исходного массива
     void outputArray (int arr[]) {
         String outArray = "Исходный массив: ";
@@ -13,7 +17,7 @@ class BubbleSort {
     void bubbleSortArray(int arr[]) {
         outputArray(arr);
         for (int x = 0; x < arr.length; x++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length-1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
@@ -37,7 +41,6 @@ class BubbleSort {
 public class Main {
     public static void main(String[] args) {
         int[] s = {443,4, 535, 3, 5, 67, 78, 1, 0, 525, 552525267, 737};
-        BubbleSort array = new BubbleSort();
-        array.bubbleSortArray(s);
+        BubbleSort array = new BubbleSort(s);
     }
 }
