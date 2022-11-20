d9023b321b06b2703b588a76e2857e57210ca3c3
Zahar Ulyashov <pelfox.za@gmail.com>
2022-11-20 22:06:19 +0300

    Изменил тип данных массивов и переменных

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,45 @@ import java.util.Scanner;
 class BubbleSort {
 
     //необходимые переменные
-    private int[] sourceArray;
-    private int[] sortArray;
+    private long[] sourceArray;
+    private long[] sortArray;
 
 
     //конструктор класса
-    BubbleSort (int UserArray[]) {
-        sourceArray = UserArray;
+    BubbleSort (long sourceArray[]) {
+        this.sourceArray = sourceArray;
+        outputSourceArray(this);
         bubbleSortArray(this);
+        outputSortedArray(this);
     }
 
 
     //сортировка массива пузырьком
     void bubbleSortArray(BubbleSort obj) {
         obj.sortArray = obj.sourceArray;
-        outputArray(this);
         for (int step = 0; step < obj.sortArray.length; step++) {
             for (int index = 0; index < obj.sortArray.length-1; index++) {
                 if (obj.sortArray[index] > obj.sortArray[index + 1]) {
-                    int temp = obj.sortArray[index + 1];
+                    long temp = obj.sortArray[index + 1];
                     obj.sortArray[index + 1] = obj.sortArray[index];
                     obj.sortArray[index] = temp;
                 }
             }
         }
-        outputSortedArray(this);
     }
 
 
     //вывод исходного массива
-    void outputArray (BubbleSort obj) {
+    void outputSourceArray (BubbleSort obj) {
         System.out.println("\nИсходный массив: ");
-        for (int ArrayElement : obj.sortArray) System.out.print(ArrayElement + " ");
+        for (long ArrayElement : obj.sortArray) System.out.print(ArrayElement + " ");
     }
 
 
     //вывод отсортированного массива
     void outputSortedArray(BubbleSort obj) {
         System.out.println("\nОтсортированный массив: ");
-        for (int ArrayElement : obj.sortArray) System.out.print(ArrayElement + " ");
+        for (long ArrayElement : obj.sortArray) System.out.print(ArrayElement + " ");
     }
 }
 
@@ -54,21 +54,22 @@ public class Main {
     }
 
     //запрос пользовательских данных
-    static int[] requestUserData() {
+    static long[] requestUserData() {
         Scanner requestData = new Scanner(System.in);
-        int amountElements, numbers;
+        int amountElements;
+        long numbers;
 
         //запрос размера массива и выделение памяти под него
         do {
             System.out.println("Сколько чисел вы хотите добавить в массив?");
             amountElements = requestData.nextInt();
         } while (amountElements <= 0);
-        int[] array = new int[amountElements];
+        long[] array = new long[amountElements];
 
         //заполнение массива пользовательскими значениями
         System.out.println("Поочередно введите числа которые должны попасть в массив: ");
         for (int x = 0; x < amountElements; x++) {
-            numbers = requestData.nextInt();
+            numbers = requestData.nextLong();
             array[x] = numbers;
         }
 
